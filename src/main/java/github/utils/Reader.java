5e86f2c9dbd032ac0465413feb746c4ec4ae671f
Irena Radl <Irena.Radl@infobip.com>
2014-03-11 14:41:27 +0100

    Reader not changed

--- src/main/java/github/utils/Reader.java
+++ src/main/java/github/utils/Reader.java
@@ -1,4 +1,10 @@
 package github.utils;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -7,4 +13,9 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooToString
 @RooJpaActiveRecord
 public class Reader {
+	public String readResource(URL url) throws IOException{
+        Resource resource = new UrlResource(url);
+        return IOUtils.toString(resource.getInputStream());
+    }
+	
 }
