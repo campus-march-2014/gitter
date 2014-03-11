85d64164abb9fc379f2277c0e6b4388d07e9073a
Irena Radl <Irena.Radl@infobip.com>
2014-03-11 23:10:43 +0100

    CommitParserImpl implemented and DataPropertyType added

--- src/main/java/github/utils/impl/CommitParserImpl.java
+++ src/main/java/github/utils/impl/CommitParserImpl.java
@@ -1,7 +1,22 @@
 package github.utils.impl;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import github.models.Data;
 import github.models.DataCollection;
+import github.models.DataProperty;
+import github.models.DataPropertyType;
+import github.models.DataType;
 import github.utils.Parser;
 
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -12,9 +27,79 @@ import org.springframework.roo.addon.tostring.RooToString;
 public class CommitParserImpl implements Parser {
 
 	@Override
-	public DataCollection parseData(String stringResorces) {
-		// TODO Auto-generated method stub
+	public DataCollection parseData(String jsonString) {
 		
-		return null;
+		JsonFactory factory = new JsonFactory();
+        ObjectMapper mapper = new ObjectMapper(factory);
+        TypeReference<ArrayList<Object>> typeRef = new TypeReference<ArrayList<Object>>() {};
+        DataCollection dataCollection = new DataCollection();
+        try {
+        	ArrayList<Object> objects = mapper.readValue(jsonString, typeRef);
+    
+        	HashMap<String, Object> hashMap = (HashMap <String, Object>) objects.get(0);
+			String url = (String) hashMap.get("url");
+        	DataProperty urlProperty = new DataProperty();
+        	urlProperty.setName("CommitURL");
+        	urlProperty.setValue(url);
+        	
+        	HashMap<String, Object> commit =(HashMap<String, Object>) hashMap.get("commit");
+        	HashMap<String, Object> committer = (HashMap<String, Object >) commit.get("committer");
+            String committerName = (String) committer.get("name");
+            DataProperty committerNameProperty = new DataProperty();
+            committerNameProperty.setName("CommitterName");
+            committerNameProperty.setValue(committerName);
+            committerNameProperty.setDataPropertyType(DataPropertyType.STRING);
+            
+            String message = (String) commit.get("message");
+            DataProperty committerMessageProperty = new DataProperty();
+            committerMessageProperty.setName("CommitterMessage");
+            committerMessageProperty.setValue(message);
+            
+            Data data = createData(urlProperty, committerNameProperty,
+					committerMessageProperty);
+            
+            createDataCollection(dataCollection, url, data);
+
+            
+ } catch (JsonParseException e) {
+  // TODO Auto-generated catch block
+  e.printStackTrace();
+ } catch (JsonMappingException e) {
+  // TODO Auto-generated catch block
+  e.printStackTrace();
+ } catch (IOException e) {
+  // TODO Auto-generated catch block
+  e.printStackTrace();
+ }	
+ 
+return dataCollection;
 	}
+
+	private void createDataCollection(DataCollection dataCollection,
+			String url, Data data) {
+		ArrayList<Data> commitData = new ArrayList<Data>();
+		commitData.add(data);
+		
+		String[] words = url.split("/");
+		String chanel = words[5];
+		
+		dataCollection.setData(commitData);
+		dataCollection.setChanelName(chanel);
+	}
+
+	private Data createData(DataProperty urlProperty,
+			DataProperty committerNameProperty,
+			DataProperty committerMessageProperty) {
+		Set<DataProperty> commitPropertiesSet = new HashSet<DataProperty>();
+		commitPropertiesSet.add(urlProperty);
+		commitPropertiesSet.add(committerNameProperty);
+		commitPropertiesSet.add(committerMessageProperty);   
+		
+		Data data = new Data();
+		data.setFields(commitPropertiesSet);
+		data.setDataType(DataType.COMMIT);
+		return data;
+	}
+	
+	
 }
---
+++ src/main/java/github/utils/impl/TestParser.java
@@ -0,0 +1,30 @@
+package github.utils.impl;
+import github.utils.Reader;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestParser {
+	
+	Reader reader;
+	CommitParserImpl parser;
+	
+	@Before
+	public void setup(){
+		parser = new CommitParserImpl();
+		reader= new Reader();
+	
+	}
+	
+	@Test
+	public void test(){
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
