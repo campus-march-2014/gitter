d06dab3a4970446c7925d2c93d84ba07c8cd16f0
Irena Radl <Irena.Radl@infobip.com>
2014-03-12 18:23:34 +0100

    CommitParserImpl implemented

--- src/main/java/github/utils/impl/CommitParserImpl.java
+++ src/main/java/github/utils/impl/CommitParserImpl.java
@@ -1,4 +1,5 @@
 package github.utils.impl;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,42 +29,51 @@ public class CommitParserImpl implements Parser {
 
 	@Override
 	public DataCollection parseData(String jsonString) {
-		
+
 		JsonFactory factory = new JsonFactory();
-        ObjectMapper mapper = new ObjectMapper(factory);
-        TypeReference<ArrayList<Object>> typeRef = new TypeReference<ArrayList<Object>>() {};
-        DataCollection dataCollection = new DataCollection();
-        try {
-        	ArrayList<Object> objects = mapper.readValue(jsonString, typeRef);
-        	HashMap<String, Object> hashMap = (HashMap <String, Object>) objects.get(0);
-        	
-			String url = (String) hashMap.get("url");
-        	DataProperty urlProperty = createUrlDataProperty(url);
-        	
-        	HashMap<String, Object> commit =(HashMap<String, Object>) hashMap.get("commit");
-        	HashMap<String, Object> committer = (HashMap<String, Object >) commit.get("committer");
-            String committerName = (String) committer.get("name");
-            DataProperty committerNameProperty = createDataNameProperty(committerName);
-            
-            String message = (String) commit.get("message");
-            DataProperty committerMessageProperty = createCommitterMessageProperty(message);
-            
-            Data data = createData(urlProperty, committerNameProperty,
-					committerMessageProperty);
-            createDataCollection(dataCollection, url, data);
-           
- } catch (JsonParseException e) {
-  // TODO Auto-generated catch block
-  e.printStackTrace();
- } catch (JsonMappingException e) {
-  // TODO Auto-generated catch block
-  e.printStackTrace();
- } catch (IOException e) {
-  // TODO Auto-generated catch block
-  e.printStackTrace();
- }	
- 
-return dataCollection;
+		ObjectMapper mapper = new ObjectMapper(factory);
+		TypeReference<ArrayList<Object>> typeRef = new TypeReference<ArrayList<Object>>() {
+		};
+		DataCollection dataCollection = new DataCollection();
+		try {
+
+			ArrayList<Object> objects = mapper.readValue(jsonString, typeRef);
+
+			for (int i = 0; i < objects.size(); i++) {
+
+				HashMap<String, Object> hashMap = (HashMap<String, Object>) objects
+						.get(i);
+				String url = (String) hashMap.get("url");
+				DataProperty urlProperty = createUrlDataProperty(url);
+
+				HashMap<String, Object> commit = (HashMap<String, Object>) hashMap
+						.get("commit");
+				HashMap<String, Object> committer = (HashMap<String, Object>) commit
+						.get("committer");
+				String committerName = (String) committer.get("name");
+				DataProperty committerNameProperty = createDataNameProperty(committerName);
+
+				String message = (String) commit.get("message");
+				DataProperty committerMessageProperty = createCommitterMessageProperty(message);
+
+				Data data = createData(urlProperty, committerNameProperty,
+						committerMessageProperty);
+				createDataCollection(dataCollection, url, data);
+
+			}
+
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return dataCollection;
 	}
 
 	private DataProperty createCommitterMessageProperty(String message) {
@@ -88,31 +98,31 @@ return dataCollection;
 		return urlProperty;
 	}
 
-	private void createDataCollection(DataCollection dataCollection,
-			String url, Data data) {
-		ArrayList<Data> commitData = new ArrayList<Data>();
-		commitData.add(data);
-		
-		String[] words = url.split("/");
-		String chanel = words[5];
-		
-		dataCollection.setData(commitData);
-		dataCollection.setChanelName(chanel);
-	}
-
 	private Data createData(DataProperty urlProperty,
 			DataProperty committerNameProperty,
 			DataProperty committerMessageProperty) {
 		Set<DataProperty> commitPropertiesSet = new HashSet<DataProperty>();
+
 		commitPropertiesSet.add(urlProperty);
 		commitPropertiesSet.add(committerNameProperty);
-		commitPropertiesSet.add(committerMessageProperty);   
-		
+		commitPropertiesSet.add(committerMessageProperty);
+
 		Data data = new Data();
 		data.setFields(commitPropertiesSet);
 		data.setDataType(DataType.COMMIT);
 		return data;
 	}
-	
-	
+
+	private void createDataCollection(DataCollection dataCollection,
+			String url, Data data) {
+		ArrayList<Data> commitData = new ArrayList<Data>();
+		commitData.add(data);
+
+		String[] words = url.split("/");
+		String chanel = words[5];
+
+		dataCollection.setData(commitData);
+		dataCollection.setChanelName(chanel);
+	}
+
 }
